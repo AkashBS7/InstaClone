ad4d6c11b7d511604c97d4be4c73b99d11daf106
SumeetShelar <56186357+SumeetShelar@users.noreply.github.com>
2021-12-23 22:16:58 +0530

    added padding and cursor pointer

--- src/components/LoginScreen/LoginPage.jsx
+++ src/components/LoginScreen/LoginPage.jsx
@@ -11,8 +11,8 @@ export default function LoginPage() {
             <div className='px-12 mt-10 bg-white flex justify-center items-center flex-col mb-5 border rounded-md shadow-md'>
                 <div className='my-4'><img src="https://see.fontimg.com/api/renderfont4/ZVGqm/eyJyIjoiZnMiLCJoIjoxMDgsInciOjEyNTAsImZzIjo4NiwiZmdjIjoiIzAwMDAwMCIsImJnYyI6IiNGRkZGRkYiLCJ0IjoxfQ/SW5zdGFncmFt/amsterdam.png" alt="Instagram" width={155} height={155}/></div>
                 <div className='flex flex-col '>
-                    <input type='text' className='border h-10 w-64 rounded bg-gray-100 mb-2'placeholder='Phone number, username, or email'/>
-                    <input type='text' className='border h-10 w-64 rounded bg-gray-100 mb-4' placeholder='Password'/>
+                    <input type='text' className='border h-10 w-64 rounded bg-gray-100 mb-2 pl-1' placeholder='Phone number, username, or email'/>
+                    <input type='password' className='border h-10 w-64 rounded bg-gray-100 mb-4 pl-1' placeholder='Password'/>
                 </div>
                 <div>
                     <button className='LoginBtn w-64 h-8 rounded text-white mb-8'>Log In</button>
@@ -23,21 +23,21 @@ export default function LoginPage() {
                 </div>
 
                 <div>
-                    <p className='fbtxt text-md mb-6'><p className="fa fa-facebook"></p>Log in with Facebook</p>
+                    <p className='fbtxt text-md mb-6 cursor-pointer'><p className="fa fa-facebook"></p>Log in with Facebook</p>
                 </div>
 
-                <p className='text-xs mb-8'>Forgot password?</p>
+                <p className='text-xs mb-8 cursor-pointer'>Forgot password?</p>
             </div>
 
             <div className='px-16 bg-white h-16 flex justify-center items-center border rounded-md shadow-md'>
-                <p className='flex'>Don't have an account? <p className='ml-2 text-blue-600 font-medium'>Sign up</p></p>
+                <span className='flex'>Don't have an account? <p className='ml-2 text-blue-600 font-medium cursor-pointer'>Sign up</p></span>
             </div>
 
             <div className='my-4'>Get the app.</div>
 
             <div className='flex'>
-                <img src={appstorePng} alt='appstore' width={120} height={50} className='mr-4 shadow-md'/>
-                <img src={googlePng} alt='googlestore' width={120} height={50} className='shadow-md'/>
+                <img src={appstorePng} alt='appstore' width={120} height={50} className='mr-4 shadow-md cursor-pointer'/>
+                <img src={googlePng} alt='googlestore' width={120} height={50} className='shadow-md cursor-pointer'/>
             </div>
         </div>
     )
